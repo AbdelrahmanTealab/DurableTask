3af94482afa121f173ddd10f2b413c68290a0d26
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2022-08-15 23:07:32 -0400

    Prepare file structure

---
+++ src/assets/components/HomeContent.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class HomeContent extends Component {
+  render() {
+    return (
+      <View>
+        <Text>HomeContent</Text>
+      </View>
+    )
+  }
+}
+
+export default HomeContent
